fd0e4497a18a94f08fd4f15c2cf356bb9c455c9a
sunil.shakya <sunil.shakya>
2023-08-28 17:10:58 +0530

    add workflow

---
+++ services/HelloWorld/src/HelloWorld/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using MiaServiceDotNetLibrary;
+using MiaServiceDotNetLibrary.Decorators;
+using MiaServiceDotNetLibrary.Environment;
+using MiaServiceDotNetLibrary.Logging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore_Hello_World_Microservice_Example.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HelloWorldController : ServiceStatusController
+    {
+        public HelloWorldController(MiaEnvConfiguration miaEnvConfiguration, ServiceClientFactory serviceClientFactory,
+            DecoratorResponseFactory decoratorResponseFactory) : base(miaEnvConfiguration, serviceClientFactory,
+            decoratorResponseFactory)
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            Logger.Info(HttpContext.Request, "starting hello api");
+            var miaHeadersPropagator = (MiaHeadersPropagator) HttpContext.Items["MiaHeadersPropagator"];
+            return $"Hello {miaHeadersPropagator.GetUserId()}";
+        }
+    }
+}

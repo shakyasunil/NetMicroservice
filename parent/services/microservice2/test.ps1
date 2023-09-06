c579a6224562dde80f0db3a2a08fd9c13907b08e
sunil.shakya <sunil.shakya>
2023-09-07 01:31:44 +0530

    update

--- parent/services/microservice2/test.ps1
+++
@@ -1,8 +0,0 @@
-$CurrentDirectory = Get-Location
-
-Get-ChildItem $CurrentDirectory -directory | Where{$_.name -match "(.*)$"}  | ForEach{[pscustomobject]@{'base'=$matches[1]}} | group base | ForEach{($_.Name)} | Select -Last 1
-
-
-
-$version=Get-ChildItem $CurrentDirectory -directory | Where{$_.name -match "(.*)$"} | ForEach{[pscustomobject]@{'base'=$matches[1];'version'=$matches[2]}} | group base | ForEach{($_.Name)+($_.group|select -last 1 -ExpandProperty version)} | Select -Last 1
-echo $version
